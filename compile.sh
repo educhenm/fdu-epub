76e4417debe4b851bfb71b6691efc74d9f00f56c
chrisfinazzo <chris.finazzo@gmail.com>
2014-06-07 15:11:21 -0400

    EPUB 3

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc -S --epub-metadata=metadata.xml -o book.epub cover.html abstract.html book.html references.html
+pandoc -S -t epub3 --epub-metadata=metadata.xml -o book.epub cover.html abstract.html book.html references.html
